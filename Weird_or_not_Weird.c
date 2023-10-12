4d67933fd0c98f367da93178abd9700c12623779
Chopra-14 <147584011+Chopra-14@users.noreply.github.com>
2023-10-12 12:03:17 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     if(n%2 != 0 && 6<n<20)     {         printf("weird");     }     else     {         printf("not weird");     } }

---
+++ Weird_or_not_Weird.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    if(n%2 != 0 && 6<n<20)
+    {
+        printf("weird");
+    }
+    else 
+    {
+        printf("not weird");
+    }
+}
