53fe0317b878be4c377d21ea157737fc0a021cc1
Chopra-14 <147584011+Chopra-14@users.noreply.github.com>
2023-11-21 11:28:46 +0530

    #include<stdio.h> int main() {     char i,j;     int n;     scanf("%d",&n);     for(i=n;i>=1;i--)     {         for(j=i;j<=n;j++)         {             printf("%c ",64+i);         }         printf(" ");     } }

---
+++ Basic_Pattern_2.5.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    char i,j;
+    int n;
+    scanf("%d",&n);
+    for(i=n;i>=1;i--)
+    {
+        for(j=i;j<=n;j++)
+        {
+            printf("%c ",64+i);
+        }
+        printf("
+");
+    }
+}
