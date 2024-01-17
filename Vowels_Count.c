cd593a889f4fea62d77abe6a9400b0240e99a4fd
Chopra-14 <147584011+Chopra-14@users.noreply.github.com>
2024-01-17 10:55:29 +0530

    #include <stdio.h> int countVowels(char str[]) {     int count = 0;     for (int i = 0; str[i] != NULL; i++) {         if (str[i] == 'a' || str[i] == 'e' || str[i] == 'i' || str[i] == 'o' || str[i] == 'u' ||             str[i] == 'A' || str[i] == 'E' || str[i] == 'I' || str[i] == 'O' || str[i] == 'U') {             count++;         }     }     return count; } int main() {     char str[100];     scanf("%[^ ]", str);     int vowelCount = countVowels(str);     printf("%d ", vowelCount);     return 0; }

---
+++ Vowels_Count.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int countVowels(char str[]) {
+    int count = 0;
+    for (int i = 0; str[i] != NULL; i++) {
+        if (str[i] == 'a' || str[i] == 'e' || str[i] == 'i' || str[i] == 'o' || str[i] == 'u' ||
+            str[i] == 'A' || str[i] == 'E' || str[i] == 'I' || str[i] == 'O' || str[i] == 'U') {
+            count++;
+        }
+    }
+    return count;
+}
+int main() {
+    char str[100];
+    scanf("%[^
+]", str);
+    int vowelCount = countVowels(str);
+    printf("%d
+", vowelCount);
+    return 0;
+}
