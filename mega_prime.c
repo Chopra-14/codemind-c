5c807c8b3b3c766f54bc75e73020d7304542d9cc
Chopra-14 <147584011+Chopra-14@users.noreply.github.com>
2024-02-14 14:40:24 +0530

    #include <stdio.h> int is_prime(int num) {     if (num <= 1) {         return 0;     }     for (int i = 2; i * i <= num; i++) {         if (num % i == 0) {             return 0;         }     }     return 1; } int is_mega_prime(int num) {     if (!is_prime(num)) {         return 0;     }     while (num > 0) {         int digit = num % 10;         if (!is_prime(digit)) {             return 0;         }         num /= 10;     }     return 1; } int main() {     int num;     scanf("%d", &num);     if (is_mega_prime(num)) {         printf("Mega Prime ");     } else {         printf("Not Mega Prime ");     }     return 0; }

--- mega_prime.c
+++ mega_prime.c
@@ -1,46 +1,37 @@
 #include <stdio.h>
-#include <stdbool.h>
-
-bool isPrime(int num) {
+int is_prime(int num) {
     if (num <= 1) {
-        return false;
+        return 0;
     }
-    
     for (int i = 2; i * i <= num; i++) {
         if (num % i == 0) {
-            return false;
+            return 0;
         }
     }
-    
-    return true;
+    return 1;
 }
-
-bool isMegaPrime(int num) {
-    if (!isPrime(num)) {
-        return false;
+int is_mega_prime(int num) {
+    if (!is_prime(num)) {
+        return 0;
     }
-    
     while (num > 0) {
         int digit = num % 10;
-        if (!isPrime(digit)) {
-            return false;
+        if (!is_prime(digit)) {
+            return 0;
         }
         num /= 10;
     }
-    
-    return true;
+    return 1;
 }
-
 int main() {
     int num;
     scanf("%d", &num);
-    if (isMegaPrime(num)) {
+    if (is_mega_prime(num)) {
         printf("Mega Prime
 ");
     } else {
         printf("Not Mega Prime
 ");
     }
-    
     return 0;
 }
